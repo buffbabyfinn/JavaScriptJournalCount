eb384168227daf7410517878ce7203260e2525ea
Joey Arnstein and Megan Fayer <student@epicodus.com>
2016-03-14 10:11:34 -0700

    build a project with node, gulp, browserify, javascript functionality

--- js/cli.js
+++ js/cli.js
@@ -1,6 +1,10 @@
-var pingPong = require('./ping-pong.js').pingPong;console.log('Ping-ponging up to 100!');
+var pingPong = require('./ping-pong.js').pingPong;
+var prompt = require('prompt');
+prompt.start();
 
-var result = pingPong(100);
-result.forEach(function(element) {
-  console.log(element);
+prompt.get('goal', function(err, result) {
+  var output = pingPong(result.goal);
+  output.forEach(function(element) {
+    console.log(element);
+  });
 });
